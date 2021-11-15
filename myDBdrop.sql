24411028b25f45fbacb4a594294c758a505e2e4b
Avadhut <avadhut.talbar@gmail.com>
2021-11-15 12:12:39 +0530

    Update myDBdrop.sql

--- myDBdrop.sql
+++ myDBdrop.sql
@@ -1,1 +1,25 @@
+Drop table F21_S003_7_Person  cascade constraints;
 
+Drop table F21_S003_7_Person_email cascade constraints; 
+
+Drop table F21_S003_7_Person_Phonenumber cascade constraints; 
+
+Drop table F21_S003_7_Customer cascade constraints; 
+
+Drop table F21_S003_7_Employee cascade constraints; 
+
+Drop table F21_S003_7_Department cascade constraints; 
+
+Drop table F21_S003_7_RestaurantBranch cascade constraints; 
+
+Drop table F21_S003_7_Dishes cascade constraints; 
+
+Drop table F21_S003_7_Dishes_Ingredients cascade constraints; 
+
+Drop table F21_S003_7_Expenses cascade constraints; 
+
+Drop table F21_S003_7_Orders cascade constraints; 
+
+Drop table F21_S003_7_Transactions cascade constraints; 
+
+Drop table F21_S003_7_RestaurantBranch_Dishes cascade constraints; 
