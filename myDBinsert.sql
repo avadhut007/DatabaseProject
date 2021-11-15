137463771b03c95b5f0f868d3439697ea4f785da
omkar-9905 <51113480+omkar-9905@users.noreply.github.com>
2021-11-15 14:29:10 -0600

    Update myDBinsert.sql

--- myDBinsert.sql
+++ myDBinsert.sql
@@ -1,1 +1,189 @@
+----- PERSON ---------------------
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Emmye' ,'Derkes' , '20-AUG-1988', '018 Armistice Plaza Dallas');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Alick' ,'Mc Pake' , '30-SEP-1980', '68461 Union Junction Atlanta');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Rebecka' ,'Hanks' , '08-SEP-1985', '2026 Muir Crossing Tampa');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Dorey' ,'Stichel' , '03-JAN-1981', '2026 Muir Crossing Seattle');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Levey' ,'Marion' , '22-FEB-1982', '2 Golf Hill Detroit');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Guy' ,'Elings' , '26-APR-1985', '56 Brickson Park Hill Houston');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Katherine' ,'Bliven' , '30-JUL-1993', '47 Express Road Miami');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Inez' ,'Boyda' , '12-JUN-1996', '34805 Sutteridge Pass Chicago');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Gabbie' ,'Fentem' , '01-MAY-1998', '6 Melvin Lane Washington');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Dniren' ,'Pettengell' , '19-MAR-1990', '5 Melrose Avenue Boston');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Layen' ,'Warre' , '25-OCT-1991', '3 Buhler Drive Phoenix');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Anni' ,'Gandey' , '23-DEC-1986', '39619 Ramsey Park Detroit');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Fredric' ,'pleuman' , '21-SEP-1989', '83673 Hoard Way Seattle');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('jarib' ,'Jowitt' , '05-MAR-1984', '382 Scoville Place San Fransisco');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Benton' ,'Lusher' , '10-FEB-1994', '757 Jay Alley Boston');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Ceclio' ,'hurry' , '13-AUG-1999', '432 Fallview Point Dallas');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Allyn' ,'Leftwitch' , '01-APR-2000', '622 Duke Lane Hoston');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Theodosia' ,'Dinse' , '07-NOV-1991', '3 Westridge Center Washington');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Lefty' ,'Broader' , '09-OCT-1984', '8072 Lighthouse Bay Way Phoenix');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Clarinda' ,'Binge' , '12-FEB-1988', '616 Marquette Pass Miami');
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Helli' ,'Hamil' , '27-JAN-1989', '3 Buhler Drive Houston');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Bert' ,'Jagg' , '31-AUG-1983', '5 Melrose Avenue Atlanta');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Nev' ,'Jenkin' , '28-SEP-1980', '34805 Sutteridge Pass Tampa');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Tev' ,'Issac' , '13-JAN-1987', '2026 Muir Crossing Dallas');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Teja' ,'Niblo' , '22-FEB-1983', '33 Prentice Trail Detroit');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Marj' ,'Slow' , '26-APR-1981', '96 Old Shore Crossing Houston');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Doll' ,'Fast' , '30-JUL-1994', '9771 Caliangt Street Miami');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Uins' ,'Soden' , '12-JUN-1992', '515 Helena Terrace Chicago');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Doule' ,'Jankin' , '01-MAY-1996', '08889 Superior Lane Washington');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Gol' ,'Bree' , '12-APR-1991', '29072 Paget Circle Phoneix');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Gal' ,'Ladel' , '17-JUL-1995', '3254 Bashford Plaza Boston');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Tonny' ,'Pird' , '11-FEB-1980', '181 Elgar Trail Detroit');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('jon' ,'Karus' , '29-SEP-1989', '6 Bunting Alley San Fransisco');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Glow' ,'Davio' , '02-APR-1984', '8833 Marquette Lane Seattle');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Flow' ,'Artois' , '11-JUN-1994', '8118 Myrtle Road Boston');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Donnie' ,'Bower' , '13-AUG-2000', '914 Lawn Avenue Dallas');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Any' ,'Dur' , '01-APR-1996', '0684 Hallows Terrace Hoston');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Ceaser' ,'Chotty' , '07-NOV-1992', '4 Northridge Junction Washington');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Bran' ,'Gipp' , '09-NOC-1983', '594 Haas Pass Phoenix');
+Insert into F21_S003_7_Person(fname, lname, dob, address) values ('Joby' ,'Kell' , '14-OCT-1982', '92252 Bayside Road Boston');
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+----- PERSON _EMAIL---------------------
+insert into F21_S003_7_Person_email(fname, lname, email) values ('Emmye', 'Derkes', 'ederkes0@google.com');
+insert into F21_S003_7_Person_email(fname, lname, email) values ('Alick', 'Mc Pake', 'amcpake1@de.vu');
+insert into F21_S003_7_Person_email(fname, lname, email) values ('Rebecka', 'Hanks', 'rhanks2@google.com');
+insert into F21_S003_7_Person_email(fname, lname, email) values ('Dorey', 'Stichel', 'dstichel3@alexa.com');
+insert into F21_S003_7_Person_email(fname, lname, email) values ('Levey', 'Marion', 'lmar4@theardian.com');
+insert into F21_S003_7_Person_email(fname, lname, email) values ('Guy', 'Elings', 'gengs5@bigctel.com');
+insert into F21_S003_7_Person_email(fname, lname, email) values ('Katherine', 'Bliven', 'kbliven6@redff.com');
+insert into F21_S003_7_Person_email(fname, lname, email) values ('Inez', 'Boyda', 'iboyda7@wp.com');
+insert into F21_S003_7_Person_email(fname, lname, email) values ('Gabbie', 'Fentem', 'gfm8@blinst.com');
+insert into F21_S003_7_Person_email(fname, lname, email) values ('Dniren', 'Pettengell', 'dpel9@jimdo.com');
+insert into F21_S003_7_Person_email(fname, lname, email) values ('Layen', 'Warre', 'lwarrea@wisc.edu');
+insert into F21_S003_7_Person_email(fname, lname, email) values ('Anni', 'Gandey', 'ageyb@tinyurl.com');
+insert into F21_S003_7_Person_email(fname, lname, email) values ('Fredric', 'pleuman', 'fpumanc@wsj.com');
+insert into F21_S003_7_Person_email(fname, lname, email) values ('jarib', 'Jowitt', 'jjowtd@sohu.com');
+insert into F21_S003_7_Person_email(fname, lname, email) values ('Benton', 'Lusher', 'blushere@mail.ru');
+insert into F21_S003_7_Person_email(fname, lname, email) values ('Ceclio', 'hurry', 'churryf@openrce.org');
+insert into F21_S003_7_Person_email(fname, lname, email) values ('Allyn', 'Leftwitch', 'aleftwichg@hp.com');
+insert into F21_S003_7_Person_email(fname, lname, email) values ('Theodosia', 'Dinse', 'tdinseh@hc360.com');
+insert into F21_S003_7_Person_email(fname, lname, email) values ('Lefty', 'Broader', 'loaderi@washton.edu');
+Insert into F21_S003_7_Person_email(fname, lname,  email) values ('Clarinda' ,'Binge' , '12-FEB-1988', 'jyoliver@hotmail.com');
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Insert into F21_S003_7_Person_email(fname, lname,  email) values ('Helli' ,'Hamil' , '27-JAN-1989', 'michiel@me.com');
+Insert into F21_S003_7_Person_email(fname, lname,  email) values ('Bert' ,'Jagg' , '31-AUG-1983', 'noahb@mac.com');
+Insert into F21_S003_7_Person_email(fname, lname,  email) values ('Nev' ,'Jenkin' , '28-SEP-1980', 'jimmichie@aol.com');
+Insert into F21_S003_7_Person_email(fname, lname,  email) values ('Tev' ,'Issac' , '13-JAN-1987', '2caidaperl@comcast.nets');
+Insert into F21_S003_7_Person_email(fname, lname,  email) values ('Teja' ,'Niblo' , '22-FEB-1983', 'tandersbr@verizon.net');
+Insert into F21_S003_7_Person_email(fname, lname,  email) values ('Marj' ,'Slow' , '26-APR-1981', 'cparis@me.com');
+Insert into F21_S003_7_Person_email(fname, lname,  email) values ('Doll' ,'Fast' , '30-JUL-1994', 'avalon@mac.com');
+Insert into F21_S003_7_Person_email(fname, lname,  email) values ('Uins' ,'Soden' , '12-JUN-1992', 'gknauss@sbcglobal.net');
+Insert into F21_S003_7_Person_email(fname, lname,  email) values ('Doule' ,'Jankin' , '01-MAY-1996', 'massignjumbo@msn.com');
+Insert into F21_S003_7_Person_email(fname, lname,  email) values ('Gol' ,'Bree' , '12-APR-1991', 'catchsuspicious@live.com');
+Insert into F21_S003_7_Person_email(fname, lname,  email) values ('Gal' ,'Ladel' , '17-JUL-1995', 'awkwardliz@att.net');
+Insert into F21_S003_7_Person_email(fname, lname,  email) values ('Tonny' ,'Pird' , '11-FEB-1980', 'horrifiedtrudi@mac.com');
+Insert into F21_S003_7_Person_email(fname, lname,  email) values ('jon' ,'Karus' , '29-SEP-1989', 'leavegrowing@me.com');
+Insert into F21_S003_7_Person_email(fname, lname,  email) values ('Glow' ,'Davio' , '02-APR-1984', 'stunnedjonty@sbcglobal.net');
+Insert into F21_S003_7_Person_email(fname, lname,  email) values ('Flow' ,'Artois' , '11-JUN-1994', 'janeyconsiderate@verizon.net');
+Insert into F21_S003_7_Person_email(fname, lname,  email) values ('Donnie' ,'Bower' , '13-AUG-2000', 'nikkiconsiderate@yahoo.com');
+Insert into F21_S003_7_Person_email(fname, lname,  email) values ('Any' ,'Dur' , '01-APR-1996', 'leavegrowing@me.com');
+Insert into F21_S003_7_Person_email(fname, lname,  email) values ('Ceaser' ,'Chotty' , '07-NOV-1992', 'leavegrowing@me.com');
+Insert into F21_S003_7_Person_email(fname, lname,  email) values ('Bran' ,'Gipp' , '09-NOC-1983', 'barragehonorable@live.com');
+Insert into F21_S003_7_Person_email(fname, lname,  email) values ('Joby' ,'Kell' , '14-OCT-1982', 'reteachdirect@yahoo.com');
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+----- PERSON _PHONENUMBER---------------------
+insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Emmye', 'Derkes', '3599704313');
+insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Alick', 'Mc Pake', '3299704413');
+insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Rebecka', 'Hanks', '2599704513');
+insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Dorey', 'Stichel', '7596704313);
+insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Levey', 'Marion', '1599304343');
+insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Guy', 'Elings', '3345974437');
+insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Katherine', 'Bliven', '3495754313');
+insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Inez', 'Boyda', '3898707313');
+insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Gabbie', 'Fentem', '339504643');
+insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Dniren', 'Pettengell', '3799704718');
+insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Layen', 'Warre', '3594704313');
+insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Anni', 'Gandey', '354594513');
+insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Fredric', 'pleuman', '3599503442');
+insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('jarib', 'Jowitt', '2599705111');
+insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Benton', 'Lusher', '2599701111');
+insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Ceclio', 'hurry', '4599704222');
+insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Allyn', 'Leftwitch', '3599704333');
+insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Theodosia', 'Dinse', '3599702323');
+insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Lefty', 'Broader', '3599504303');
+Insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Clarinda' ,'Binge' ,'9124362195');
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Helli' ,'Hamil' , '27JAN1989', '9393294853');
+Insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Bert' ,'Jagg' , '31AUG1983', '2034796926');
+Insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Nev' ,'Jenkin' , '28SEP1980', '2143071484');
+Insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Tev' ,'Issac' , '13JAN1987', '8569358942');
+Insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Teja' ,'Niblo' , '22FEB1983', '5122220282');
+Insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Marj' ,'Slow' , '26APR1981', '6179390744');
+Insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Doll' ,'Fast' , '30JUL1994', '7134893015');
+Insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Uins' ,'Soden' , '12JUN1992', '6469771006');
+Insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Doule' ,'Jankin' , '01MAY1996', '9402059690');
+Insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Gol' ,'Bree' , '12APR1991', '2098930751');
+Insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Gal' ,'Ladel' , '17JUL1995', '4349829338');
+Insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Tonny' ,'Pird' , '11FEB1980', '6193652819');
+Insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('jon' ,'Karus' , '29SEP1989', '7704268358');
+Insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Glow' ,'Davio' , '02APR1984', '9204830033');
+Insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Flow' ,'Artois' , '11JUN1994', '6145150492');
+Insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Donnie' ,'Bower' , '13AUG2000', '8187754983');
+Insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Any' ,'Dur' , '01APR1996', '5802776746');
+Insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Ceaser' ,'Chotty' , '07NOV1992', '9186685985');
+Insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Bran' ,'Gipp' , '09NOC1983', '3193573235');
+Insert into F21_S003_7_Person_phonenumber(fname, lname, phonenumber) values ('Joby' ,'Kell' , '14OCT1982', '6089850121');
+
+---------------------------------------------------------------------------------------------------------
+----- Customer ---------------------
+
+Insert into F21_S003_7_Customer(fname, lname, customerid) values('Helli', 'Hamil', 501);
+Insert into F21_S003_7_Customer(fname, lname, customerid) values('Alick', 'Mc Pake', 502);
+Insert into F21_S003_7_Customer(fname, lname, customerid) values('Rebecka', 'Hanks', 503);
+Insert into F21_S003_7_Customer(fname, lname, customerid) values('Dorey', 'Stichel', 504);
+Insert into F21_S003_7_Customer(fname, lname, customerid) values('Levey', 'Marion', 505);
+Insert into F21_S003_7_Customer(fname, lname, customerid) values('Guy', 'Elings', 506);
+Insert into F21_S003_7_Customer(fname, lname, customerid) values('Bert', 'Jagg', 507);
+Insert into F21_S003_7_Customer(fname, lname, customerid) values('Nev', 'Jenkin', 508);
+Insert into F21_S003_7_Customer(fname, lname, customerid) values('Tev', 'Issac', 509);
+Insert into F21_S003_7_Customer(fname, lname, customerid) values('Teja, 'Niblo, 510);
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+----- DEPARTMENT---------------------
+--Chicago 10:--
+Insert into F21_S003_7_Department(deptid, deptname, branchname, managerid) values(101,	'Cleaning staff', 'Chicago',1);
+Insert into F21_S003_7_Department(deptid, deptname, branchname, managerid) values(102, 'Managerial staff', 'Chicago', 2);
+Insert into F21_S003_7_Department(deptid, deptname, branchname, managerid) values(103, 'Kitchen staff', 'Chicago', 3);
+Insert into F21_S003_7_Department(deptid, deptname, branchname, managerid) values(104, 'Accounting staff', 'Chicago', 4);
+Insert into F21_S003_7_Department(deptid, deptname, branchname, managerid) values(105, 'HR staff', 'Chicago', 5);
+
+
+--Miami 20:
+Insert into F21_S003_7_Department(deptid, deptname, branchname, managerid) values(201,	'Cleaning staff', 'Miami',6);
+Insert into F21_S003_7_Department(deptid, deptname, branchname, managerid) values(202, 'Managerial staff', 'Miami', 7);
+Insert into F21_S003_7_Department(deptid, deptname, branchname, managerid) values(203, 'Kitchen staff', 'Miami', 8);
+Insert into F21_S003_7_Department(deptid, deptname, branchname, managerid) values(204, 'Accounting staff', 'Miami', 9);
+Insert into F21_S003_7_Department(deptid, deptname, branchname, managerid) values(205, 'HR staff', 'Miami', 10);
+
+
+--Dallas:30
+Insert into F21_S003_7_Department(deptid, deptname, branchname, managerid) values(301,	'Cleaning staff', 'Dallas',11);
+Insert into F21_S003_7_Department(deptid, deptname, branchname, managerid) values(302, 'Managerial staff', 'Dallas', 12);
+Insert into F21_S003_7_Department(deptid, deptname, branchname, managerid) values(303, 'Kitchen staff', 'Dallas', 13);
+Insert into F21_S003_7_Department(deptid, deptname, branchname, managerid) values(304, 'Accounting staff', 'Dallas', 14);
+Insert into F21_S003_7_Department(deptid, deptname, branchname, managerid) values(305, 'HR staff', 'Dallas', 15);
+
+--Boston: 40
+--Insert into F21_S003_7_Department(deptit, deptname, branchname, managerid) values (401  ,’Administrator’ , ‘Boston’ , 201  );
+--Insert into F21_S003_7_Department(deptit, deptname, branchname, managerid) values (402  ,’Cleaning staff’ , ‘Boston’ , 202  );
+--Insert into F21_S003_7_Department(deptit, deptname, branchname, managerid) values (403  ,’Managerial staff’ ,  ‘Boston’ , 203  );
+--Insert into F21_S003_7_Department(deptit, deptname, branchname, managerid) values (404  ,’Kitchen staff’ ,  ‘Boston’ , 204  );
+--Insert into F21_S003_7_Department(deptit, deptname, branchname, managerid) values (405  ,’Accounting staff’ ,  ‘Boston’ , 205  );
+--Insert into F21_S003_7_Department(deptit, deptname, branchname, managerid) values (406  ,’HR staff’ ,  ‘Boston’ , 206  );
+
+/*Department names: 
+Managerial staff
+Waiter/cleaning staff
+Administrator staff
+Kitchen staff
+Accounting staff
+HR staff
+Training staff
+Marketing Staff
+*/
 
