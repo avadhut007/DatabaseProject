ea3df1b56468e898593f0f5772ea782c5e9748b4
Avadhut <avadhut.talbar@gmail.com>
2021-11-16 22:33:49 +0530

    Added 2nd goal

--- myDBqueries.sql
+++ myDBqueries.sql
@@ -1,1 +1,15 @@
 
+-- Business Goal 2 --
+-- Branch with highest Revenue 
+
+select r.name, r.branchid, rev.revenue from F21_S003_7_RestaurantBranch r inner join
+(select branchid, sum(price) as revenue
+from F21_S003_7_Orders o , F21_S003_7_Transactions t
+where o.customerid = t.customerid
+group by rollup(branchid)
+order by revenue desc) rev
+on r.branchid = rev.branchid
+fetch first 1 row only;
+
+
+
